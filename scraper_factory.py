bb39dfefb78e6a3b26706a471f4c54bbf0ff5f18
Justin <89544777+JustinLuft@users.noreply.github.com>
2025-06-19 22:01:27 -0400

    Update scraper_factory.py

--- scraper_factory.py
+++ scraper_factory.py
@@ -1,6 +1,6 @@
 from typing import Dict, Type, List
-from .base_scraper import BaseScraper
-from .alpha_capital_scraper import AlphaCapitalScraper
+from base_scraper import BaseScraper
+from alpha_capital_scrapper import AlphaCapitalScraper
 
 class ScraperFactory:
     """Enhanced factory class with better website matching"""
